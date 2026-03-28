36f2ef4c535e8419c0a01bc56fddb1fda0331a0f
kimi <lroe@cedarville.edu>
2026-03-28 07:07:43 -0400

    delete unnecessary file

--- app/api/mqtt/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    url: process.env.MQTT_URL,
-    username: process.env.MQTT_USERNAME,
-    password: process.env.MQTT_PASSWORD,
-  });
-}
