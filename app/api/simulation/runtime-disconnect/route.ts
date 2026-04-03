cf4665dbb572dd922e375812bbd498d363ea57d4
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2026-04-03 11:12:33 -0400

    Have a working way to run simulation
    Data is not sent to car yet. Not every data point is connected to car data for running simulation, but we are close.

---
+++ app/api/simulation/runtime-disconnect/route.ts
@@ -0,0 +1,25 @@
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const res = await fetch("http://localhost:8000/runtime_dis", {
+    //PYTHON_API_URL=https://internal-api.myapp.com
+    //PYTHON_API_URL=http://python-service:8000
+    //PYTHON_API_URL=http://localhost:8000
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const text = await res.text();
+
+  if (!res.ok) {
+    return new Response(text, { status: res.status });
+  }
+
+  return new Response(text, {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,16 @@
 
 import BackButton from "@/components/ui/backButton"
 import { useMqtt } from "@/hooks/use-mqtt"
-import { Suspense, useMemo } from "react"
+import { Suspense, useMemo, useState } from "react"
 import { useSearchParams, useRouter } from "next/navigation"
+import { pyRuntimeConnect, pyRunSimulation, pyRuntimeDisconnect } from "./runPython";
 
 function DashboardContent() {
   const searchParams = useSearchParams()
   const router = useRouter()
-  
+
+  const [runtimeConnected, setRuntimeConnected] = useState(false);
+  const [estFuelCost, setEstFuelCost] = useState(0);
   const selectedCar = searchParams.get('car') || 'karch'
 
   const mqttOptions = useMemo(() => ({
@@ -33,6 +36,37 @@ function DashboardContent() {
       return null;
     }
   }, [lastMessage]);
+
+  async function runSimulation(){
+    console.log("Running Simulation.")
+    // console.log(carData.temp, carData.pressure);
+    // console.log(carData.speed, carData.distance_traveled, carData.time, carData.selectedCar, carData.fuel, carData.burnsUsed, carData);
+    if(carData){ // carData needs to exist
+      // track length for Shell: 2.39072566 * 5280 ft (4 laps total)
+      //TODO - what units is distance_traveled? does this calculation need to be adjusted (track length in ft)
+      const lapNum = Math.ceil(carData.distance_traveled / (2.39072566 * 5280)); //hardcoded for Shell Track
+      console.log("Lap Number: ", lapNum);
+      const result = await pyRunSimulation("simulation_data", "Gold Lightning II", "Ima Placeholder", 
+                                           "indy", 70, 14.6957, carData.speed, carData.time, estFuelCost, 0, lapNum);
+      console.log("Simulation Finished.");
+      setEstFuelCost(result[0]);
+      const simData = result[1];
+      //TODO - How do I send the data to the car.
+      console.log("Data: " + simData[0]);
+    }
+  };
+
+  async function connectRuntime(){
+    const connected = await pyRuntimeConnect();
+    setRuntimeConnected(connected);
+    console.log("Connection Result: " + connected); 
+  };
+
+  async function disconnectRuntime(){
+    const connected = await pyRuntimeDisconnect();
+    setRuntimeConnected(connected);
+    console.log("Connection Result: " + connected); 
+  };
 
   return (
     <div style={{ padding: '2rem', position: 'relative'}}>
@@ -64,6 +98,28 @@ function DashboardContent() {
       <p>
         Time: {carData ? carData.time : "--"} 
       </p>
+      <button
+        disabled={runtimeConnected}
+        onClick={ connectRuntime }
+        className="w-full bg-red-600 hover:bg-red-600/90 disabled:opacity-50 py-3 rounded-lg font-semibold"
+      >
+        Connect to MATLAB Runtime
+      </button>
+      <button
+        onClick={ runSimulation }
+        disabled={!runtimeConnected}
+        className="w-full bg-red-600 hover:bg-red-600/90 disabled:opacity-50 py-3 rounded-lg font-semibold"
+      >
+        Run Simulation
+      </button>
+      <button
+        disabled={!runtimeConnected}
+        onClick={ disconnectRuntime }
+        className="w-full bg-red-600 hover:bg-red-600/90 disabled:opacity-50 py-3 rounded-lg font-semibold"
+      >
+        Disconnect MATLAB Runtime
+      </button>
+      
     </div>
   );
 }
